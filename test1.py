5d037d41f5af7e35f570f456e7eaa02a71edafd6
fordude1 <hauns@fros.com>
2017-06-24 23:54:06 -0700

    Playing with comments and variables some more

--- test1.py
+++ test1.py
@@ -1,5 +1,11 @@
 # These are just some initial tests - nothing really exciting here
 # for my UCSC Python class
+
+# Print Hello
 print "Hello!"
+
+# Assign the 42 to the variable answer
 answer = 42
+
+# Print the results
 print "The Answer is: ", answer
