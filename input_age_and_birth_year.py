09545e38320d3425e8fc17dc023a783c41120563
fordude1 <hauns@fros.com>
2017-06-25 16:36:01 -0700

    Add comments and inform user of exit status

--- input_age_and_birth_year.py
+++ input_age_and_birth_year.py
@@ -8,19 +8,22 @@
 age_input = raw_input("What is your age? ")
 # catch exceptions
 try:
+    # make sure to convert to an integer
     age = int(age_input)
 except ValueError:
     print "The number input", age_input, "is not a valid integer"
+    print "The program will now exit!"
 else:
         print "The number input", age_input, "is a valid integer."
 
 # Inquire year born from user
         year_born_input = raw_input("What year were you born? ")
         try:
+            # make sure to convert to an integer
             year = int(year_born_input)
         except:
             print "The number input ",year_born_input, "is not a valid integer."
-            print "Program will now exit."
+            print "Program will now exit!"
         else:
             print "The number input",year_born_input, "is a valid integer."
             print "You are", age, "and you were born in", year, "."
