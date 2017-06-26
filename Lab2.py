775358938174dc57ae17b1faebf6a98480f2100a
fordude1 <hauns@fros.com>
2017-06-25 20:39:06 -0700

    Syntax corrections

--- Lab2.py
+++ Lab2.py
@@ -1,16 +1,16 @@
 # Lab 2 from Week 1
 
-# Question 1 Find the modulor for the following
+# Question 1 Find the modulo for the following
 print "Lab 2, Question 1:"
-print "0 modulo 5 = ",(0%5)
-print "1 modulo 5 = ",(1%5)
-print "2 modulo 5 = ",(2%5)
-print "3 modulo 5 = ",(3%5)
-print "4 modulo 5 = ",(4%5)
-print "5 modulo 5 = ",(5%5)
-print "6 modulo 5 = ",(6%5)
-print "7 modulo 5 = ",(7%5)
-print "8 modulo 5 = ",(8%5)
+print "0 modulo 5 = ", (0%5)
+print "1 modulo 5 = ", (1%5)
+print "2 modulo 5 = ", (2%5)
+print "3 modulo 5 = ", (3%5)
+print "4 modulo 5 = ", (4%5)
+print "5 modulo 5 = ", (5%5)
+print "6 modulo 5 = ", (6%5)
+print "7 modulo 5 = ", (7%5)
+print "8 modulo 5 = ", (8%5)
 print
 # Question 2 Write a program that asks the user for an integer
 # Determine if the integer is odd or even
