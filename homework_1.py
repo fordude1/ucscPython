045c920bfd8115c0699faf94ff74ffd3385ac1f4
fordude1 <hauns@fros.com>
2017-06-26 19:46:14 -0700

    Homework 1 initial commit

---
+++ homework_1.py
@@ -0,0 +1,16 @@
+# Week 1, Homework 1
+#
+# Write a program that asks the user for their dream job title.  Then ask them for the
+# hourly wage they want to earn.  This number should account for dollars and cents.
+# Calculate the annual wage they will earn over a year.  Assume they work 48 weeks a
+# year (4 weeks vacation) but are paid over 52 weeks.  Then ask them how much
+# money they feel they will need at retirement time.  Determine the number of years
+# they need to save is an even or odd numbers.  For this part, use the modulus operator
+# to determine if the number you calculated is and even or odd.  Once you have calculated
+# it, use print to print out the value.  You should also print out something like this.  "If
+# the value is --- then the number is odd, however, if the value is ---- then the number
+# is even."
+# Make sure you account for user input error.  Test your code by trying to cause errors
+# in input.  It is not important to error check on their dream job title.  Just assume it
+# will be a reasonable string.
+
