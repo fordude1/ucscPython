dd9ad35663acc021cd2017ea378c4173746af5f9
fordude1 <hauns@fros.com>
2017-06-26 19:40:40 -0700

    Add another way to divide floating numbers (cast)

--- l2q5.py
+++ l2q5.py
@@ -15,4 +15,9 @@ print "If you specify float, you will get the correct answer: "
 print "4 / (2 * 1.0) = ", 4 / (2 * 1.0)
 print "5 / (2 * 1.0) = ", 5 / (2 * 1.0)
 
+print "Another way to do this is: "
+print "4 / float(2) =", 4 / float(2)
+print "5 / float(2) =", 5 / float(2)
+
+
 
