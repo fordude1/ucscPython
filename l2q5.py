d0f70e70797bc56b6a6c1d8d16211c39743181a8
fordude1 <hauns@fros.com>
2017-06-26 19:38:27 -0700

    Add lab 2 question 5.

---
+++ l2q5.py
@@ -0,0 +1,18 @@
+# Write a program or use the interpreter to
+# determine the result of:
+# 4/2 =
+# 5/2 =
+# Are these answers accurate?
+# How could you make them more accurate?
+
+# HINT: c = a / (b * 1.0)
+
+print "If you don't specify float in either name or number, you get this: "
+print "4 / 2 = ", 4 / 2
+print "5 / 2 = ", 5 / 2
+
+print "If you specify float, you will get the correct answer: "
+print "4 / (2 * 1.0) = ", 4 / (2 * 1.0)
+print "5 / (2 * 1.0) = ", 5 / (2 * 1.0)
+
+
