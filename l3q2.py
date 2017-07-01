974f8dfb9cf6fcbe6e315c1ab7a8bf38e939c0af
Hauns <hauns@fros.com>
2017-07-01 07:47:10 -0700

    had problems detecting non integer values.  Finally figured out that the test statement had to be inside the try.

--- l3q2.py
+++ l3q2.py
@@ -6,18 +6,32 @@ will keep on asking the user for the number until it is
 valid.
 '''
 
+#while True:
+#    age = raw_input('Give me your age; type "quit" to end.\n')
+
+#    if age == 'quit':
+#        break
+#    try:
+
+#        int_age = int(age)
+#        print 'Your age is', int_age
+#    except:
+#        ValueError
+#        print 'Your age must be an integer value'
+
 while True:
     # Request user input
-    num_request = raw_input('Please provide a number that is greater than 0:')
+    positive_integer_request = raw_input('Please provide a number that is greater than 0:\n')
 
-    if int(num_request) > 0:
-        print 'The number you entered was', num_request
-        break
     try:
-            num_request = int(num_request)
-            print num_request, 'is not greater than 0.'
+            int_number = int(positive_integer_request)
+            print 'The number you provided was:', int_number
+            if int_number > 0:
+                print 'This is greater than 0'
+                break
     except:
             ValueError
-            print 'Your input must be an integer.'
+            print 'Your input must be an integer.  You entered' , positive_integer_request
+
 
 
