35e9785fee751023ffbc3d6f33a83e686c772df3
Hauns <hauns@fros.com>
2017-07-01 07:57:21 -0700

    commit lab 3 question 3

---
+++ l3q3.py
@@ -0,0 +1,16 @@
+'''
+Review the following statements.  If there is an error, please indicate where
+the error exists and how to correct it.
+if x < 5 or x = 6:
+if x < 0 and x > 5:
+if i == 12 or i not 6:
+'''
+
+# 1
+if x < 5 or x == 6:
+
+# 2 x can't be 2 numbers at the same time - this isn't quantum computing
+if x < 0 or x > 5:
+
+# 3
+if i == 12 or i != 6:
