7f3f29c4366f24ad9c01753d059b91335269dada
Hauns <hauns@fros.com>
2017-06-30 21:47:21 -0700

    Add sum of numbers

---
+++ sum1to100.py
@@ -0,0 +1,21 @@
+'''
+sum_1_to_100_while_loop.py
+
+The following small script calculates the sum of
+the numbers from 1 to 100
+
+'''
+
+number_to_count_to = 100
+
+sum_of_numbers = 0
+initial_value = 1
+
+while initial_value <= number_to_count_to:
+
+    # Print current count and value
+    print initial_value, sum_of_numbers
+    sum_of_numbers = sum_of_numbers + initial_value
+    initial_value = initial_value + 1
+
+print "Sum of 1 until", number_to_count_to, "is", sum_of_numbers
