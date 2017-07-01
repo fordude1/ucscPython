441505ca5da4433be7ffcef2b9aaf117bd89dc4c
Hauns <hauns@fros.com>
2017-06-30 22:08:24 -0700

    Add lab3 question 1

---
+++ l3q1.py
@@ -0,0 +1,18 @@
+'''
+Conditional Statements combined with operators (comparison or relational,
+assignment, arithmetic, and logical).  For each sentence, translate the
+sentence to Python code using your operators:
+
+if i is from 2 to 78, subtract 2 from i
+
+if j is not between 10 and 15 but is the
+number 11, then k equals j times 2
+
+if k is not equal to 7 and i equals 5 or j
+is greater than 8, set x equal to 200
+divided by k
+
+if i is the sum of j and k, and j is not
+equal to sum of i and k, then x equals j
+times k divided by i
+'''
