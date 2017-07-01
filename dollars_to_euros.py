c7f956e55206c2ed53efc8162fd3170236d37e50
Hauns <hauns@fros.com>
2017-07-01 13:36:17 -0700

    commit dollars_to_euros

---
+++ dollars_to_euros.py
@@ -0,0 +1,48 @@
+'''
+Convert_dollars_to_euros.py
+
+use of docstrings:  A docstring is a string
+literal that occurs as the first statement in
+a module, functin, class, or method definition.
+
+Program using functions that repeatedly asks user
+for valid US dollars amount (float) and once valid
+amount given will convert that to euros using the
+current exchange rate at time of writing
+
+7/1/2017 - $1.00 = 0.88 Euro
+
+'''
+
+EURO_CONVERSION_RATE = 1.88
+
+def GetDollars():
+    """Asks user for dollar amount as float
+    and returns a verified float
+    """
+    while True:
+        us_dollars = raw_input('Enter dollar and cents value to convert.\n')
+        try:
+            float_us_dollars = float(us_dollars)
+        except:
+            ValueError
+            print us_dollars, "is not a valid dollar amount.  Try again."
+            continue
+        return float_us_dollars
+
+def ConvertDollarsEuros(us_dollars):
+    """Converts the parameter us_dollars to euros
+    """
+    euros = us_dollars / EURO_CONVERSION_RATE
+
+    return euros
+
+def Run():
+    """ Run function that will run GetDollars and ConvertDollarsEuros
+    functions and print out the result
+    """
+    verified_us_dollars = GetDollars()
+    print '''You will be converting''', verified_us_dollars, '''dollars to euros.\n'''
+    print "$", verified_us_dollars, '''converted to euros is''', ConvertDollarsEuros(verified_us_dollars)
+
+Run()
