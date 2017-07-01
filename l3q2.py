0bf72f447d8256e72de768425d8777f9fac932d3
Hauns <hauns@fros.com>
2017-07-01 12:23:01 -0700

    Add some comments for future reference

--- l3q2.py
+++ l3q2.py
@@ -10,9 +10,11 @@ while True:
     # Request user input
     positive_integer_request = raw_input('Please provide a number that is greater than 0:\n')
 
+    # Look for exceptions
     try:
             int_number = int(positive_integer_request)
             print 'The number you provided was:', int_number
+            # Nest the number test here to prevent exceptions for non integer input
             if int_number > 0:
                 print 'This is greater than 0'
                 break
