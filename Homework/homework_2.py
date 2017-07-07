34be3b081b52696cda8d8b6d516c785c3b2d22ca
Hauns <hauns@fros.com>
2017-07-06 17:02:01 -0700

    test homework

--- Homework/homework_2.py
+++ Homework/homework_2.py
@@ -18,15 +18,15 @@ def AskForLetter():
 
     while True:
         letter = raw_input('Please input a single letter.\n')
-        if letter_request == 'quit':
+        if letter == 'quit':
             break
-        if len(letter_request) == 0 or len(letter_request) > 1:
+        if len(letter) == 0 or len(letter) > 1:
             print "you didn't enter anything or more than 1 letter"
+
         else:
-            print "you entered 1 letter"
+            print "you entered a number."
             # return the value of the letter
             return IsVowel(letter)
-
 
 def IsVowel(letter):
     '''
@@ -36,30 +36,41 @@ def IsVowel(letter):
     if letter is vowel or not, this function will call a third and a fourth function below.
     '''
 
-
+    if letter == "a" or "A" or "e" or "E" or "i" or "I" or "o" or "O" or "u" or "U":
+        return IsLowercaseVowel(letter)
+    else:
+        print "letter is consonant"
 
 def IsLowercaseVowel(letter):
     '''
     this function will take a single variable as input called letter and will
     return True if the letter is a lowercase vowel and will return False if not.
-    :param letter:
-    :return:
     '''
+
+    if letter.islower():
+        return PrintIsVowel(letter)
 
 
 def IsUppercaseVowel(letter):
     '''
     this function will take a single variable as input called letter
     and will return True if the letter is a uppercase vowel and will return False if not.
-    :param letter:
-    :return:
     '''
 
-def PrintIsVowel():
+    if letter.isupper():
+        print letter, "is lower case vowel"
+        return False
+
+def PrintIsVowel(letter):
     '''
     this function will print a sentence to the user (standard out) indicating if the input is a vowel or not.
     It will be called inside AskForLetter(letter).
     This function will take as input the original letter input by the user
     so that it can be included in the output sentence.
-    :return:
     '''
+
+    print "The letter you typed", "'",letter,"'" "is a vowell."
+
+
+# Start the program
+AskForLetter()
