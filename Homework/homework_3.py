a0cf263d688945cb480005b02fa9afa79c0f473a
Hauns <hauns@fros.com>
2017-07-11 21:02:03 -0700

    skeleton

--- Homework/homework_3.py
+++ Homework/homework_3.py
@@ -4,6 +4,42 @@
 
 '''
 Write a program that takes a 3 word phrase and then converts the words to Pig Latin.
+start if list is 3 words - if not, have them re-enter
 '''
 
-VOWELS =
+# define the constant named VOWELS
+VOWELS = ["a","e","i","o","u"]
+
+# if letter in vowels
+
+def AskUserForSentence ():
+    '''
+    obtain 3 word phrase from user
+    :return:
+    '''
+
+
+def LowercaseSentence ():
+    '''
+    convert the sentence to lower case
+    :return:
+    '''
+
+def SplitSentenceIntoList ():
+    '''
+    split the sentence into a list
+    :return:
+    '''
+
+def ConvertWordToPigLatin ():
+    '''
+    convert each word to pig latin
+    :return:
+    '''
+
+def PrintThreeWordPhrase ():
+    '''
+    print the phrase in pig latin
+    :return:
+    '''
+
