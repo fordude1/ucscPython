2a3b1d2080343e54152d28200ab5aff4e30a248e
Hauns <hauns@fros.com>
2017-07-11 20:41:05 -0700

    homework 3 start

---
+++ Homework/homework_3.py
@@ -0,0 +1,9 @@
+# Hauns Froehlingsdorf
+# Python Programming for Beginners
+# Week 3 Homework
+
+'''
+Write a program that takes a 3 word phrase and then converts the words to Pig Latin.
+'''
+
+VOWELS =
