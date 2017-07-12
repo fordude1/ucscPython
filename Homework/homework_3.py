51240ff3520795011158433368012b0e1bd08127
Hauns <hauns@fros.com>
2017-07-11 21:55:45 -0700

    lowercasesentence working

--- Homework/homework_3.py
+++ Homework/homework_3.py
@@ -15,18 +15,25 @@ VOWELS = ["a","e","i","o","u"]
 def AskUserForSentence ():
     '''
     obtain 3 word phrase from user
-    :return:
+    :return:phrase
     '''
+
     # initialize our list
     user_input = []
-    # a while loop seams logical to get user input and watch for quit to exit
+
+    # a while loop seams logical to get user input and watch for QUIT to exit
     while True:
-        user_input = raw_input("Please type a 3 word phrase to conver to pig latin.  type 'QUIT' to exit program.")
+        user_input = raw_input("Please type a 3 word phrase to convert to pig latin.  type 'QUIT' to exit program.")
         if user_input == "QUIT":
             break
-        # convert input to string
+
+        # convert input to string seperated by space
         phrase = user_input.split(" ")
+
+        # check if phrase has 3 words
         if len(phrase) == 3:
+
+            # send the proper input to LowercaseSentence for case change
             LowercaseSentence(phrase)
 
 def LowercaseSentence (phrase):
@@ -34,7 +41,19 @@ def LowercaseSentence (phrase):
     convert the sentence to lower case
     :return:
     '''
-    print "Your phrase is:",phrase
+
+    # initialize lowerphrase
+    lowerphrase = []
+
+    # use a for loop to convert each word to lowercase
+    for word in phrase:
+        case_word = word.lower()
+        # add the lower case word to the list
+        lowerphrase.append(case_word)
+
+    print lowerphrase
+
+
 
 def SplitSentenceIntoList ():
     '''
