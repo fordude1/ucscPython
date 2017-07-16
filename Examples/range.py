9e8d331458332b1432faa15c3d16655005c64172
Hauns <hauns@fros.com>
2017-07-16 15:12:39 -0700

    add range example

---
+++ Examples/range.py
@@ -0,0 +1,26 @@
+# Demonstrating the range function
+
+'''
+range_examples
+'''
+
+print 'One paramater - range(6)'
+
+for i in range(6):
+    print i
+
+print 'Two paramaters - range (3, 6)'
+
+for i in range(3, 6):
+    print i
+
+print 'Three Paramagers - range (4, 10, 2)'
+
+for i in range(4, 10, 2):
+    print i
+
+print 'Going backwards - range (0, -10, -2)'
+
+for i in range(0, -10, -2):
+    print i
+
