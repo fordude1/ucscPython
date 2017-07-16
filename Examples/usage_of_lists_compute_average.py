1e3c1b8c71c0857f034d9873702a78e41201ac8a
Hauns <hauns@fros.com>
2017-07-16 15:59:22 -0700

    commit file

--- Examples/usage_of_lists_compute_average.py
+++ Examples/usage_of_lists_compute_average.py
@@ -5,18 +5,18 @@ to use lists to calculate the average of homework grades.
 Also introduces the built-in sum() function.
 '''
 
-def CalculateAverage(grade_list):
 
+def CalculateAverage(grade_list):
     sum_grades = sum(grade_list)
     ave = (sum_grades) / len(grade_list)
 
     return ave
 
-def GetGrades(grade_list):
 
+def GetGrades(grade_list):
     average = CalculateAverage(grade_list)
 
     print "Your homework average is", average
 
-GetGrades([100, 90, 85, 99, 82])
 
+GetGrades([100, 90, 85, 99, 82])
