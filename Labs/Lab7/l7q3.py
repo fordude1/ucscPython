9471ff08300cd276fd198bd7a06a63f1fef94203
Hauns <hauns@fros.com>
2017-07-16 15:22:33 -0700

    add lab 7 exercise 3

---
+++ Labs/Lab7/l7q3.py
@@ -0,0 +1,14 @@
+'''
+Try the below in the interpreter
+'''
+
+for char in 'Python':
+    print char
+
+list_1 = ['GO','PACK','GO!']
+for chant in list_1:
+    print chant,
+
+for item in [12,'Rodgers',[1929, 1930, 1931, 1939, 1944, 1961, 1962, 1965, 1966, 1967, 1996, 2010]]:
+    print item
+
