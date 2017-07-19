714f115d46e617ab2dd98399c1dadefaeb93aad5
fordude1 <hauns@fros.com>
2017-07-18 18:30:59 -0700

    add enumeration example

---
+++ Examples/enumerate_over_list.py
@@ -0,0 +1,13 @@
+'''
+enumerate_over_list.py - shows how you can enumerate over a list
+'''
+
+def EnumerateList (list_1):
+
+    for i, item in enumerate(list_1):
+        print 'index is', i
+        print 'item in list is', item
+
+EnumerateList( ['Aaron Rodgers single season passer rating', 122.5, \
+              2011, [' Rodgers', 'Manning', 'Foles', 'Brady'] ])
+
