a12c8aea075fe7c273f47acf1bc3e49327bdffe6
Hauns <hauns@fros.com>
2017-07-26 17:35:19 -0700

    homework 5 solution file initial commit

---
+++ Homework/homework_5_solution_program.py
@@ -0,0 +1,7 @@
+# Hauns Froehlingsdorf
+# Python Programming for Beginners
+# Week 5, Homework 5 part 2
+
+#  determine the differences between two files after first writing a new file
+# that is exactly the same as the input file.
+# This program will be importing homework_5_solution_module.py.
